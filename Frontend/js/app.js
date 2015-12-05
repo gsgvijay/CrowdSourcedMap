f4a24836e464b0c967ce9bf89b31ea7a1d105960
Vijay Sankar Ganesh <vijay@localhost.localdomain>
2015-12-04 23:38:07 -0500

    Incomplete. Second iteration.

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -1,9 +1,18 @@
 var myApp = angular.module("myApp", ['ngRoute', 'ngMap', 'myAppController']);
 
+myApp.run(function($rootScope) {
+    $rootScope.register = 'http://localhost:5000/register';
+    $rootScope.getEvents = 'http://localhost:5000/get_events';
+    $rootScope.postEvent = 'http://localhost:5000/post_event';
+    $rootScope.cancelEvent = 'http://localhost:5000/cancel_event';
+    $rootScope.user = 'vgsankar';
+});
+
 myApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/mainView', {
-           templateUrl: 'partials/mainView.html'
+            templateUrl: 'partials/mainView.html',
+            controller = 'initializeCtrl'
         }).
         when('/create', {
             templateUrl: 'partials/create.html',
@@ -12,11 +21,20 @@ myApp.config(['$routeProvider', function($routeProvider) {
         when('/view', {
             templateUrl: 'partials/view.html'
         }).
-        when('/myEvents', {
-            templateUrl: 'partials/myEvents.html'
+        when('/myProfile', {
+            templateUrl: 'partials/myProfile.html'
         }).
-        when('/circles', {
-            templateUrl: 'partials/circles.html'
+        when('/myRatings', {
+            templateUrl: 'partials/myProfile.html',
+            controller: 'ratingsCtrl'
+        }).
+        when('/myEvents', {
+            templateUrl: 'partials/myProfile.html',
+            controller: 'myEventsCtrl'
+        }).
+        when('/myCircles', {
+            templateUrl: 'partials/myProfile.html',
+            controller: 'myCirclesCtrl'
         }).
         otherwise({
             redirectTo: '/mainView'
--- Frontend/js/controller.js
+++ Frontend/js/controller.js
@@ -1,7 +1,24 @@
 var myAppControllers = angular.module('myAppController', []);
 
-myAppControllers.controller('createCtrl', ['$scope', function($scope) {
+myAppControllers.controller('createCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+    $scope.user = $rootScope.user;
     $scope.createNewEvent = function() {
         console.log('Created');
     };
+}]);
+
+myAppControllers.controller('initializeCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+    // Check if the user exists
+}]);
+
+myAppControllers.controller('ratingsCtrl', ['$scope', function($scope) {
+    $scope.text = 'These are my ratings';
+}]);
+
+myAppControllers.controller('myEventsCtrl', ['$scope', function($scope) {
+    $scope.text = 'These are my events';
+}]);
+
+myAppControllers.controller('myCirclesCtrl', ['$scope', function($scope) {
+    $scope.text = 'These are my circles';
 }]);
